380797031d46cb18040bdfc45022d204f0b2bc9f
wangxing02 <xing.wang@skylarklabs.ai>
2023-04-22 00:58:01 +0000

    Update for evaluation

--- run.py
+++ run.py
@@ -59,13 +59,19 @@ def enroll_images(src_dir):
     return features
 
 
-def main():
-    enroll_dir = './datasets/Enroll'
+def evaluate_video():
+    enroll_dir = '/home/ubuntu/Work/datasets/Custom_Dataset_Test_Environment/Reference_People_Dataset_100'
     features = enroll_images(enroll_dir)
 
+    video_file = "./datasets/testing_2022-08-09_15-33-03.144797.mp4"
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    fps = 15
+    frame_size = (1280, 1440)
+    out = cv2.VideoWriter(os.path.basename(video_file), fourcc, fps, frame_size)
+
     threshold = 0.5
-    cv2.namedWindow("Face Recognition Demo")
-    cap = cv2.VideoCapture("./datasets/office.mp4")
+
+    cap = cv2.VideoCapture(video_file)
     while not cap.isOpened():
         cap = cv2.VideoCapture("./out.mp4")
         cv2.waitKey(1000)
@@ -73,6 +79,9 @@ def main():
 
     while True:
         flag, frame = cap.read()
+        if frame is None:
+            break
+
         show_img = frame.copy()
         bboxes = get_bbox(frame)
         for bbox in bboxes:
@@ -99,7 +108,9 @@ def main():
         if flag:
             # The frame is ready and already captured
             scaled_image = cv2.resize(show_img, (int(show_img.shape[1] * 0.5), int(show_img.shape[0] * 0.5)))
-            cv2.imshow('Face Recognition Demo', show_img)
+            print("Image Show Size: ", show_img.shape)
+            out.write(show_img)
+            cv2.imshow('Face Recognition Demo', scaled_image)
         else:
             # It is better to wait for a while for the next frame to be ready
             break
@@ -107,6 +118,53 @@ def main():
         if cv2.waitKey(10) == 27:
             break
 
+    # Release video capture and writer objects, and close all windows
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+
+
+def evaluate():
+    src_dir = '/home/ubuntu/Work/datasets/Custom_Dataset_Test_Environment/Raw_Dataset/Indoor'
+    files = get_file_names(src_dir)
+    threshold = 0.75
+    correct_num = 0
+    total_num = 0
+
+    for i, src_file in enumerate(files):
+        src_label = os.path.basename(os.path.dirname(src_file))
+        src_img = cv2.imread(src_file)
+        bboxes = get_bbox(src_img)
+
+        if not bboxes:
+            continue
+
+        for bbox in bboxes:
+            src_feat = get_feature_from_image(net, bbox, device, src_img)
+        print(src_label.lower())
+
+        for j, dst_file in enumerate(files[i:]):
+            dst_label = os.path.basename(os.path.dirname(dst_file))
+            dst_img = cv2.imread(dst_file)
+            bboxes = get_bbox(dst_img)
+            if not bboxes:
+                continue
+
+            print(dst_label.lower())
+            for bbox in bboxes:
+
+                dst_feat = get_feature_from_image(net, bbox, device, dst_img)
+
+                score = match_feature(src_feat, dst_feat)
+                if (src_label == dst_label and score > threshold) or (src_label != dst_label and score < threshold):
+                    correct_num += 1
+                    break
+
+            total_num += 1
+
+    print(f"Accuracy: {correct_num * 100.0 / total_num:.02}")
+
 
 if __name__ == '__main__':
-    main()
+    evaluate_video()
+    # evaluate()
